1776af5c5a748effb26593c42f2b2be6f4d2857b
CastaraAgroEconomy <r.ai.infused.2023@gmail.com>
2024-10-07 22:55:23 -0400

    Update Clear_screen.py
    Test

--- Clear_screen.py
+++ Clear_screen.py
@@ -2,5 +2,5 @@ import streamlit as st
 
 # Function to clear the display
 def clear_display():
-    reset_screen = st.empty()
-    return reset_screen
+    placeholder = st.empty()
+    return placeholder
