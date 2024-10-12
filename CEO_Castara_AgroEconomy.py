248d9930ca9a9502f5d38fc320c851c9e6b0084d
RAI2023 <r.ai.infused.2023@gmail.com>
2024-10-12 14:05:30 -0400

    Update CEO_Castara_AgroEconomy.py
    Test 9 updates

--- CEO_Castara_AgroEconomy.py
+++ CEO_Castara_AgroEconomy.py
@@ -88,7 +88,7 @@ def main_menu():
 # Display content based on selected option
 def display_content():
     #st.header(f"{st.session_state.user_role} - {st.session_state.option}")
-    st.write(f"Displaying content for {st.session_state.user_role}'s {st.session_state.option}.")
+    st.write(f"Displaying content for {st.session_state.user_role} & {st.session_state.option}.")
     
     if st.session_state.user_role == "Franchisee":
         if st.session_state.option == "Yield Management":
