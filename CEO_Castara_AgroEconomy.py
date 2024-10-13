500fa8a1195633edf7d522022df52b5567dac9a0
RAI2023 <r.ai.infused.2023@gmail.com>
2024-10-13 11:40:55 -0400

    Update CEO_Castara_AgroEconomy.py
    Test 9 updates

--- CEO_Castara_AgroEconomy.py
+++ CEO_Castara_AgroEconomy.py
@@ -136,11 +136,10 @@ def main():
     if not st.session_state.logged_in: 
         login_screen() # presents the login screen
     elif not st.session_state.user_role:
-        role_selection_screen() # request a user_role
-    
-    if st.session_state.option = None
-    option = st.session_state.option
-        main_menu(user_role) # selects from a choice of options for a given user_role
+        role_selection_screen() # request a user_role  
+    if st.session_state.option == None:
+        option = st.session_state.option
+        main_menu(user_role) # selects from a choice of options for a given user_role   
     else:
         user_role = st.session_state.user_role
         option = st.session_state.option
