e9e43111f60325a9595a9bce19188bc470cdf685
RAI2023 <r.ai.infused.2023@gmail.com>
2024-10-13 00:42:27 -0400

    Update CEO_Castara_AgroEconomy.py
    Test 9 updates

--- CEO_Castara_AgroEconomy.py
+++ CEO_Castara_AgroEconomy.py
@@ -141,7 +141,7 @@ def main():
         main_menu() # selects from a choice of options for a given user_role
     else:
         user_role = st.session_state.user_role
-        optiom = st.session_state.option
+        option = st.session_state.option
         clear_display()
         display_dashboard_internal(user_role, option) # defaults to current user role and option
 
