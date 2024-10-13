8e1b418eb9e524202beeb94a34238a2f49a39c83
RAI2023 <r.ai.infused.2023@gmail.com>
2024-10-13 09:19:30 -0400

    Update CEO_Castara_AgroEconomy.py
    Test 9 updates

--- CEO_Castara_AgroEconomy.py
+++ CEO_Castara_AgroEconomy.py
@@ -129,7 +129,7 @@ def main():
     if 'user_role' not in st.session_state:
         st.session_state.user_role = None # sets null
 # resets
-    st.session_state.clear_display = True # reset on
+    clear_display() # reset on
     if not st.session_state.logged_in: 
         login_screen() # presents the login screen
     elif not st.session_state.user_role:
