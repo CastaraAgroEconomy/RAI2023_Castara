b83ab4b6e3e448bf0e15e7ab7f58b21909e373ba
RAI2023 <r.ai.infused.2023@gmail.com>
2024-10-12 22:38:02 -0400

    Update CEO_Castara_AgroEconomy.py
    Test 9 updates

--- CEO_Castara_AgroEconomy.py
+++ CEO_Castara_AgroEconomy.py
@@ -25,10 +25,11 @@ def login_screen():
     if st.button("Login", key="login_process_button"):
         if not username or not password:
             st.error("Please enter both Username and Password.")
+        else:
             if authenticate_user(username, password):
                 st.session_state.logged_in = True
                 clear_display()
-           else
+            else:
                st.error("Authentication failed. Please check your credentials.")
 
 # Role selection screen
