db8bbd5f2f2f6aa44a020da8273bf2ed83f619ba
RAI2023 <r.ai.infused.2023@gmail.com>
2024-10-18 02:12:48 -0400

    Update CEO_Castara_AgroEconomy.py

--- CEO_Castara_AgroEconomy.py
+++ CEO_Castara_AgroEconomy.py
@@ -16,7 +16,7 @@ def login():
 # Media asset display
 def display_media_asset():
     """Display media asset on a cleared screen"""
-    st.image("features/Assets/Media/Images/Cover_page.jpg", caption="Castara AgroEconomy Venture")
+    st.image("Assets/Media/Images/Cover_page.jpg", caption="Castara AgroEconomy Venture")
 
 # Role selection
 def role_selection():
