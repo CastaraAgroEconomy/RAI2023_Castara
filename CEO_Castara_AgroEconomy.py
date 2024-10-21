cf193e692a842765b6ec0a6e3a547340d1bff5b4
RAI2023 <r.ai.infused.2023@gmail.com>
2024-10-21 08:05:42 -0400

    Update CEO_Castara_AgroEconomy.py

--- CEO_Castara_AgroEconomy.py
+++ CEO_Castara_AgroEconomy.py
@@ -115,7 +115,7 @@ def sub_role_selection(selected_role, selected_sub_role):
         
     if not st.session_state.go_action:
         selected_action = " "
-        actiion_selection(selected_role, selected_user_sub_role) # Go to activity page if go_activity not set
+        action_selection(selected_role, selected_user_sub_role) # Go to activity page if go_activity not set
     else:
         selected_activity = " "
         activity_selection(selected_role, selected_sub_user_role, selected_action, selected_activity)  # Go to activity selection page if go_activity set
