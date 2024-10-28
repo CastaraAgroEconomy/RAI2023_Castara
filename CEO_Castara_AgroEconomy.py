df69e11954c0d05fe2173ffc11fa9224b172f13d
RAI2023 <r.ai.infused.2023@gmail.com>
2024-10-28 06:46:17 -0400

    Update CEO_Castara_AgroEconomy.py

--- CEO_Castara_AgroEconomy.py
+++ CEO_Castara_AgroEconomy.py
@@ -22,8 +22,8 @@ if 'self' not in st.session_state:
 if 'R_go' not in st.session_state:
     st.session_state.R_go = False
         
-# from features.Truth_Table.truth_table_logic import gen_tab
-# from features.Validation.valid_selection import check_for_valid_match
+from features.Truth_Table.truth_table_logic import generate_table
+
 
 # Define the main function that controls the flow
 def main():
@@ -129,9 +129,7 @@ def set_stage(stage, key, value):
 
 def finalize_selection(activity):
     st.session_state.selected_activity = activity
-    # gen_tab(self, R_go, selected_role, selected_sub_role, selected_action, selected_activity)
-    # check_for_valid_match(R_go, selected_role, selected_sub_role, selected_action, selected_activity)
-    
+    generate_table(self, R_go, selected_role, selected_sub_role, selected_action, selected_activity)
 
     if 'R_go' == True:
         st.success(f"Journey completed successfully! Role={st.session_state.selected_role}, "
