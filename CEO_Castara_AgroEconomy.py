f8be22c9a3176f80bcc6f5d4f604745195172ed4
RAI2023 <r.ai.infused.2023@gmail.com>
2024-10-28 04:23:55 -0400

    Update CEO_Castara_AgroEconomy.py

--- CEO_Castara_AgroEconomy.py
+++ CEO_Castara_AgroEconomy.py
@@ -132,7 +132,7 @@ def finalize_selection(activity):
     # gen_tab(self, R_go, selected_role, selected_sub_role, selected_action, selected_activity)
     # check_for_valid_match(R_go, selected_role, selected_sub_role, selected_action, selected_activity)
 
-    if R_go == True:
+    if R_go in session_state.R_go
         st.success(f"Journey completed successfully! Role={st.session_state.selected_role}, "
         f"Sub-role={st.session_state.selected_sub_role}, Action={st.session_state.selected_action}, "
         f"Activity={st.session_state.selected_activity}") 
