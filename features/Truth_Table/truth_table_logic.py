47a2836c0c5d40a113eaba5359a8ac464cad1069
RAI2023 <r.ai.infused.2023@gmail.com>
2024-10-29 02:04:56 -0400

    Update truth_table_logic.py

--- features/Truth_Table/truth_table_logic.py
+++ features/Truth_Table/truth_table_logic.py
@@ -102,7 +102,7 @@ def validate_selection(self, R_go, selected_role, selected_sub_role, selected_ac
     # List  [D] = 35 activities
     # Table [R] = 441000 combinations
 
-    go_valid(R_go, select_role, select_sub_role, select_action, select_activity
+    go_valid(self, R_go, select_role, select_sub_role, select_action, select_activity
     
     n = roles.index(role)
     x = sub_roles[role].index(sub_role)
