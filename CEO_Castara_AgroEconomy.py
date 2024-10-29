77a251709a4f9b0ebedc678dc48c25f99b8616ed
RAI2023 <r.ai.infused.2023@gmail.com>
2024-10-29 06:22:11 -0400

    Update CEO_Castara_AgroEconomy.py

--- CEO_Castara_AgroEconomy.py
+++ CEO_Castara_AgroEconomy.py
@@ -35,7 +35,7 @@ if 'self' not in st.session_state:
 if 'R_go' not in st.session_state:
     st.session_state.R_go = 0
         
-from RAI2023-Castara.features.Truth_Table.truth_table_logic import generate_table
+from features.Truth_Table.truth_table_logic import generate_table
 
 # Define the main function that controls the flow
 def main():
