1fe0e0d38b014bbe75a59eea6a074687d3b42eff
RAI2023 <r.ai.infused.2023@gmail.com>
2024-10-30 12:07:40 -0400

    Update CEO_Castara_AgroEconomy.py

--- CEO_Castara_AgroEconomy.py
+++ CEO_Castara_AgroEconomy.py
@@ -38,7 +38,7 @@ if 'R_go' not in st.session_state:
 # from features.Truth_Table.rules_logic import generate_table
 
 # Creates and initializes the TruthTable instance
-R = TruthTable(n=14, x=30, m=30, y=35)
+# R = TruthTable(n=14, x=30, m=30, y=35)
 
 # Define the main function that controls the flow
 def main():
@@ -164,21 +164,20 @@ def finalize_selection(selected_activity):
     st.write(" ")
     st.write("⚠️ - validating selected combination ...... ")
 
-    R.generate_table(n,x,m,y)  # Generates the Truth Table
-
-    n, x, m, y = (
-       st.session_state.n,
-       st.session_state.x,
-       st.session_state.m,
-       st.session_state.y
-    )
+    # R.generate_table(n,x,m,y)  # Generates the Truth Table
+    # n, x, m, y = (
+    #   st.session_state.n,
+    #   st.session_state.x,
+    #   st.session_state.m,
+    #   st.session_state.y
+    # )
     
 
-    if validate_selection(n, x, m, y):
-        st.success("Valid combination!")
-    else:
-        next_step = R.get_next_valid_selection(n, x, m, y)
-        st.error(f"Invalid combination. Please re-select your {next_step}.")
+    # if validate_selection(n, x, m, y):
+    #    st.success("Valid combination!")
+    # else:
+    #    next_step = R.get_next_valid_selection(n, x, m, y)
+    #    st.error(f"Invalid combination. Please re-select your {next_step}.")
 
     if st.session_state.R_go == 1:
         st.success(f"Journey completed successfully! Role={st.session_state.selected_role}, "
