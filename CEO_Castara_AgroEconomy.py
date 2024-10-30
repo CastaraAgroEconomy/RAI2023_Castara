484d44eeef0b6e11052f19940ec1ad97f4711a91
RAI2023 <r.ai.infused.2023@gmail.com>
2024-10-29 23:32:22 -0400

    Update CEO_Castara_AgroEconomy.py

--- CEO_Castara_AgroEconomy.py
+++ CEO_Castara_AgroEconomy.py
@@ -105,13 +105,13 @@ def select_role(content_placeholder):
 def select_sub_role(content_placeholder):
     st.title(f"Select Sub-role for {st.session_state.selected_role}")
     
-    sub_roles = [“Head of Agricultural Engineering”, “Lead Horticulturist”, “Systems Integration Engineer”, “Plant Science Director”, 
-    “Operations Director”, “Maintenance Supervisor”, “Quality Assurance Manager”, “Harvest Team Leader”, “Environmental Systems Manager”, 
-    “Nutrient Systems Manager”, “Data Analytics Manager”, “Production Supervisor”, “Food Safety Compliance Officer”, “Automation Engineer”, 
-    “Plant Health Inspector”, “Franchise Owner”, “Regional Franchise Manager”, “Franchise Operations Director”, “Chief Investment Officer”, 
-    “Investment Manager”, “Portfolio Manager”, “Executive Director”, “Chief Operations Officer”, “Chief Financial Officer”, “Business Development Manager”, 
-    “Franchise Development Director”, “Investment Analyst”, “Financial Controller”, “Franchise Compliance Manager”, 
-    “Investor Relations Manager”]
+    sub_roles = ["Head of Agricultural Engineering", "Lead Horticulturist", "Systems Integration Engineer", "Plant Science Director", 
+    "Operations Director", "Maintenance Supervisor", "Quality Assurance Manager", "Harvest Team Leader", "Environmental Systems Manager", 
+    "Nutrient Systems Manager", "Data Analytics Manager", "Production Supervisor", "Food Safety Compliance Officer", "Automation Engineer", 
+    "Plant Health Inspector", "Franchise Owner", "Regional Franchise Manager", "Franchise Operations Director", "Chief Investment Officer", 
+    "Investment Manager", "Portfolio Manager", "Executive Director", "Chief Operations Officer", "Chief Financial Officer", "Business Development Manager", 
+    "Franchise Development Director", "Investment Analyst", "Financial Controller", "Franchise Compliance Manager", 
+    "Investor Relations Manager"]
     
     selected_sub_role = st.radio("Choose a sub-role", sub_roles)
 
