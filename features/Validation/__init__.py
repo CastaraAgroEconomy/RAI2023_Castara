615e948b36b441b7e42e48a3cb3961efeddad239
RAI2023 <r.ai.infused.2023@gmail.com>
2024-10-31 15:52:26 -0400

    Update __init__.py

--- features/Validation/__init__.py
+++ features/Validation/__init__.py
@@ -1,2 +1,2 @@
 # Enables the scripts "valid_selection.py" and "Rules.py" 
-# to be located in the "features/Validation/" subfolder
+# to be located in the "RAI2023_Castara.features/Validation/" folder.
