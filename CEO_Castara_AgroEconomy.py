a3c2df266d0d4ca7cee7c1ea5344a6d91a7b43f6
RAI2023 <r.ai.infused.2023@gmail.com>
2024-10-31 02:55:17 -0400

    Update CEO_Castara_AgroEconomy.py

--- CEO_Castara_AgroEconomy.py
+++ CEO_Castara_AgroEconomy.py
@@ -39,7 +39,7 @@ if 'R_go' not in st.session_state:
     st.session_state.R_go = 0
 
 # App session's initilization :-
-from RAI2023_Castara.features.Truth_Table.rules_logic import TruthTable
+from RAI2023_Castara.features.Truth_Table.rules_logic import generate_table
     
 # Creates and initializes the TruthTable instance
 R = TruthTable(n=14, x=30, m=30, y=35)
