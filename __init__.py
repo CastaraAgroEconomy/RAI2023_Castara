c19026b7e1eb86c4607b427b55f844aee1e0114b
RAI2023 <r.ai.infused.2023@gmail.com>
2024-10-31 15:01:28 -0400

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,2 +1,3 @@
 # Implemented to direct the Streamlit platform as to the 
 # sub-directory paths containing the various module scripts
+# Note : main folder was renamed with an underscore replacing the hyphen.
