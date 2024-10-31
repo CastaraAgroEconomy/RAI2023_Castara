ffaf24903027a28e321f054e8885ee079d8a6fc4
RAI2023 <r.ai.infused.2023@gmail.com>
2024-10-31 15:48:58 -0400

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,3 +1,4 @@
 # Implemented to direct the Streamlit platform as to the 
 # sub-directory paths containing the various module scripts
 # Note : main folder was renamed with an underscore replacing the hyphen.
+# "RAI2023-Castara" became "RAI2023_Castara"
