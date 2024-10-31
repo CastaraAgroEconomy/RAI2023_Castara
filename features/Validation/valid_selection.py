65c8efbd3299538ebf4f72dee217b94c839da678
RAI2023 <r.ai.infused.2023@gmail.com>
2024-10-31 02:26:05 -0400

    Update valid_selection.py

--- features/Validation/valid_selection.py
+++ features/Validation/valid_selection.py
@@ -3,7 +3,7 @@
 
 # import itertools
 # import pandas as pd
-from RAI2023-Castara.features.Validation.Rules import is_valid_combination
+from RAI2023_Castara.features.Validation.Rules import is_valid_combination
 
 # Define lists A, B, C, and D
 A = ["Agricultural Engineers", "Horticulturists", "System Specialist", "Plant Scientists", "Operations Managers",
