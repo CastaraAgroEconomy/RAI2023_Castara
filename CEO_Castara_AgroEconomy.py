225b8fad85f8b31bc9fd67cd5ee5c9cc033c508b
RAI2023 <r.ai.infused.2023@gmail.com>
2024-10-31 00:38:00 -0400

    Update CEO_Castara_AgroEconomy.py

--- CEO_Castara_AgroEconomy.py
+++ CEO_Castara_AgroEconomy.py
@@ -176,13 +176,7 @@ def finalize_selection(selected_activity):
        st.session_state.m,
        st.session_state.y
     )
-    
 
-    if validate_selection(n, x, m, y):
-        st.success("Valid combination!")
-    else:
-        next_step = R.get_next_valid_selection(n, x, m, y)
-        st.error(f"Invalid combination. Please re-select your {next_step}.")
 
     if st.session_state.R_go == 1:
         st.success(f"Journey completed successfully! Role={st.session_state.selected_role}, "
