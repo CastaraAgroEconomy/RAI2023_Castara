fd35a159e72950d6a14b54b436f4d3869c9c9d46
RAI2023 <r.ai.infused.2023@gmail.com>
2024-10-31 03:56:07 -0400

    Update CEO_Castara_AgroEconomy.py

--- CEO_Castara_AgroEconomy.py
+++ CEO_Castara_AgroEconomy.py
@@ -174,6 +174,7 @@ def finalize_selection(selected_activity):
     st.write("⚠️ - Selected combination to be validated .... ")
 
     R.generate_table()  # Generates the Truth Table
+    
     n, x, m, y = (
        st.session_state.n,
        st.session_state.x,
