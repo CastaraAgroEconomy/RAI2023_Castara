eafe5b807b767a5ac1c6e8610def1389c89e5d4a
RAI2023 <r.ai.infused.2023@gmail.com>
2024-11-01 03:31:50 -0400

    Update CEO_Castara_AgroEconomy.py

--- CEO_Castara_AgroEconomy.py
+++ CEO_Castara_AgroEconomy.py
@@ -101,7 +101,7 @@ def main():
             st.write("⚠️ - Currently, you will need to logout and login again, to select different combinations of options.")
             st.write("In future, the system will return you to the list which causes the first invalid combination to occur as a result of a selection from that list")
             st.write("Eventually, by the launch release, version 1.xx, only valid options will be presented based on the selected option in the prior presented list")
-            st.button("Logout", on_click=logout)
+            st.button("Logout", on_click=lambada: logout()):
             pass
             st.session_state.clear()
     return
