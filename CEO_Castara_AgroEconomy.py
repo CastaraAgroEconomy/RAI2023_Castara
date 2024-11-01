17ea8082dcce67397f83371fa40e6dc19427504c
RAI2023 <r.ai.infused.2023@gmail.com>
2024-11-01 07:11:50 -0400

    Update CEO_Castara_AgroEconomy.py

--- CEO_Castara_AgroEconomy.py
+++ CEO_Castara_AgroEconomy.py
@@ -442,7 +442,7 @@ def finalize_selection(selected_activity):
        st.session_state.y
     )
     # Was the initial 'truth' table generation call to the generation function in first module
-    generate_table(n, x, m, y) # Generates the Truth Table   
+    # generate_table(n, x, m, y) # Generates the Truth Table   
     return
 
 # Logout function
