75d727dc7e92f5cb0b96ac5db0d5bae2acb61269
RAI2023 <r.ai.infused.2023@gmail.com>
2024-11-03 19:57:24 -0400

    Update CEO_Castara_AgroEconomy.py

--- CEO_Castara_AgroEconomy.py
+++ CEO_Castara_AgroEconomy.py
@@ -207,7 +207,7 @@ def Module_1():
 
 
         def validate_choice(selected_role, selected_sub_role, selected_action, selected_activity):
-        """Validate the selection by checking if the combination matches valid rules."""
+            """Validate the selection by checking if the combination matches valid rules."""
         
             st.write(" ")
             st.write("⚠️ - Preparing to validate selected choices as a combination ")
