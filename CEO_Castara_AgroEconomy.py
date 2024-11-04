f556abaeeaba4ae27636ff9bc21ca965e537665b
RAI2023 <r.ai.infused.2023@gmail.com>
2024-11-04 17:56:47 -0400

    Update CEO_Castara_AgroEconomy.py

--- CEO_Castara_AgroEconomy.py
+++ CEO_Castara_AgroEconomy.py
@@ -248,7 +248,7 @@ def Module_1():
             st.write("⚠️ - Validating chosen items")
             st.write("     .... as a four item combination")
 
-            if validate_selection(self, n, x, m, y, selected_role, selected_sub_role, selected_action, selected_activity):
+            if self.validate_selection(n, x, m, y, selected_role, selected_sub_role, selected_action, selected_activity):
                 st.success("Valid combination!")
             else:
                 next_step = get_next_valid_selection(self, n, x, m, y)
