92838866c9dc9e09b4b4b92fcca69217fa35a005
RAI2023 <r.ai.infused.2023@gmail.com>
2024-11-07 11:15:07 -0400

    Update CEO_Castara_AgroEconomy.py

--- CEO_Castara_AgroEconomy.py
+++ CEO_Castara_AgroEconomy.py
@@ -260,8 +260,7 @@ def Module_1():
                     st.write(" ..... Access to feature granted 👍")
                     st.session_state.return_to_main = True
                     return
-            elif
-                if st.session_state.R_go == 0:
+            elif st.session_state.R_go == 0:
                    if st.session_state.n == 2 and st.session_state.y < 2:
                        st.write(" .....  Continuing to process")
                        st.session_state.return_to_main = False
