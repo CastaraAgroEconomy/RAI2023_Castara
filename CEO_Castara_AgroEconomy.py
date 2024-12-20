34928d4f72a09ef9a98b322e50351f8c6741433a
RAI2023 <r.ai.infused.2023@gmail.com>
2024-12-20 05:14:10 -0400

    Update CEO_Castara_AgroEconomy.py

--- CEO_Castara_AgroEconomy.py
+++ CEO_Castara_AgroEconomy.py
@@ -291,7 +291,7 @@ def Module_1():
                     st.write(" .....  Continuing to process")
                     st.session_state.return_to_main = False
                     return
-                if n = 14 and y = 35:
+                if n == 14 and y == 35:
                     st.write(" ..... Access to feature denied 👎")
                     st.session_state.return_to_main = True
                     return
