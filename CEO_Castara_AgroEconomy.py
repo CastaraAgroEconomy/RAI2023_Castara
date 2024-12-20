6e2df049975a7d1dcd43f64fc3dd019f77dff5b4
RAI2023 <r.ai.infused.2023@gmail.com>
2024-12-20 07:32:39 -0400

    Update CEO_Castara_AgroEconomy.py

--- CEO_Castara_AgroEconomy.py
+++ CEO_Castara_AgroEconomy.py
@@ -288,7 +288,7 @@ def Module_1():
         st.session_state.return_to_main = True
         return
     if R_go == 0:
-        if n := 14 and y != 35:
+        if n := 14 and y <= 35:
             st.write(" .....  Continuing to process")
             st.session_state.return_to_main = False
             return
