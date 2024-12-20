e0df4f2d4d203dbc279ff787691fb342793678c3
RAI2023 <r.ai.infused.2023@gmail.com>
2024-12-20 15:22:11 -0400

    Update CEO_Castara_AgroEconomy.py

--- CEO_Castara_AgroEconomy.py
+++ CEO_Castara_AgroEconomy.py
@@ -551,12 +551,12 @@ def Module_1():
 
 #           End of Nominal Rules return is_valid as 0 or 1
                         
-#   Create instance after class definition
-       st.write("debug 4 - Creating Truth Table instance"
-       truth_table = TruthTable()  # This line was missing       
+#    Create instance after class definition
+        st.write("debug 4 - Creating Truth Table instance"
+        truth_table = TruthTable()  # This line was missing       
  
 
-       st.write("debug 9: Table generation & use completed")
+        st.write("debug 9: Table generation & use completed")
     
 
 
