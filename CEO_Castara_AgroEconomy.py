606c24958b20c431b4e00e0613800a1a3b02738f
RAI2023 <r.ai.infused.2023@gmail.com>
2024-12-21 21:46:50 -0400

    Update CEO_Castara_AgroEconomy.py

--- CEO_Castara_AgroEconomy.py
+++ CEO_Castara_AgroEconomy.py
@@ -184,22 +184,21 @@ def main():
             st.write(" ")
             st.write("⚠️ - you will unfortunately be required to make new selections.")
             
-      st.session_state.logged_in = True
-      st.session_state.stage = "role_selection"
-      st.session_state.R_go = 0         
+    st.session_state.logged_in = True
+    st.session_state.stage = "role_selection"
+    st.session_state.R_go = 0         
      
- 
-      st.write(" ")
-      st.write("⚠️ - Currently, you will need to logout and login again, to select different User Role")
+    st.write(" ")
+    st.write("⚠️ - Currently, you will need to logout and login again, to select different User Role")
             
-      st.write(" ")
-      st.write("However, the system will return you to the list which causes the first invalid combination to occur, other than the User's Role, as a result of an invalid item being selected.")
-      st.write("Eventually, by the launch release, version 1.xx, only valid options will be presented based on the selected option in the prior presented list")
+    st.write(" ")
+    st.write("However, the system will return you to the list which causes the first invalid combination to occur, other than the User's Role, as a result of an invalid item being selected.")
+    st.write("Eventually, by the launch release, version 1.xx, only valid options will be presented based on the selected option in the prior presented list")
             
-      if st.button("Logout", on_click=lambda: logout()):
-           pass
-      st.session_state.stage = None
-      st.session_state.logged_in = False     
+    if st.button("Logout", on_click=lambda: logout()):
+         pass
+    st.session_state.stage = None
+    st.session_state.logged_in = False     
 
     
 # ===================
