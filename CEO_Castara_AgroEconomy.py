7b9509a74706ad9fae32b4acbcd163529fcb0901
RAI2023 <r.ai.infused.2023@gmail.com>
2024-12-22 12:33:11 -0400

    Update CEO_Castara_AgroEconomy.py

--- CEO_Castara_AgroEconomy.py
+++ CEO_Castara_AgroEconomy.py
@@ -157,9 +157,9 @@ def main():
     
     if st.session_state.R_go == 1 and st.session_state.live == 1:
         with content_placeholder.container():
-            st.success(f"Journey completed successfully! Role={st.session_state.selected_role}, "
-            f"Sub-role={st.session_state.selected_sub_role}, Action={st.session_state.selected_action}, "
-            f"Activity={st.session_state.selected_activity}") 
+            st.write(f"⚠️ - Journey completed successfully! Role={st.session_state.selected_role}")
+            st.write(f"..... Sub-role {st.session_state.selected_sub_role} can access {st.session_state.selected_action}")
+            st.write(f"..... in order to execute the mission, {st.session_state.selected_activity}")
             
             st.write(" ")
             st.write("⚠️ - When implemented, appropriate feature will activate at this point")
@@ -175,9 +175,9 @@ def main():
                     
     if st.session_state.R_go == 0 and st.session_state.live == 1:
         with content_placeholder.container():
-            st.success(f"⚠️ - Journey unsuccessfull !  The Role {st.session_state.selected_role},"
-            f"with Sub-role={st.session_state.selected_sub_role}, is not authorized to perform the action {st.session_state.selected_action}, "
-            f"in order to execute and achieve the mission {st.session_state.selected_activity}.") 
+            st.write(f"⚠️ - Journey unsuccessfull !  The Role {st.session_state.selected_role}")
+            st.write(f"..... with Sub-role={st.session_state.selected_sub_role}, is not authorized to perform the action {st.session_state.selected_action}")
+            st.write(f"..... in order to execute and achieve the mission {st.session_state.selected_activity}") 
 
             st.write(" ")
             st.write("⚠️ - you will unfortunately be required to make new selections.")
