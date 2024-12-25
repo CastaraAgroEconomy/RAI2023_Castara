419857e74ea3ad74d9d26572f5635761cc86b80e
RAI2023 <r.ai.infused.2023@gmail.com>
2024-12-25 11:09:16 -0400

    Update Compliance_Report_generatio.py

--- features/Scripts/Compliance_Report_generatio.py
+++ features/Scripts/Compliance_Report_generatio.py
@@ -1,1 +1,11 @@
 # Compliance Report generation launch pad
+import streamlit as st
+
+# Description
+st.write("This app embeds a YouTube video and plays it within the browser.")
+
+# YouTube video URL
+video_url = "https://www.Youtube.com/watch?v=PvCIl0C93zg"
+
+# Embed the video in the app
+st.video(video_url)
