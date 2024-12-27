288bfa39a2b45e94de2eea2f187e6603992a0815
RAI2023 <r.ai.infused.2023@gmail.com>
2024-12-27 15:13:06 -0400

    Update pH_Level_Monitoring.py

--- features/Scripts/pH_Level_Monitoring.py
+++ features/Scripts/pH_Level_Monitoring.py
@@ -1,11 +1,14 @@
 # pH Level Monitoring feature launch pad
 import streamlit as st
 
-# Description
-st.write("This app embeds a YouTube video and plays it within the browser.")
+def feature_script()
+#   Description
+    st.write("This app embeds a YouTube video and plays it within the browser.")
 
 # YouTube video URL
-video_url = "https://www.Youtube.com/watch?v=PvCIl0C93zg"
+    video_url = "https://www.Youtube.com/watch?v=PvCIl0C93zg"
 
 # Embed the video in the app
-st.video(video_url)
+    st.video(video_url)
+
+    return
