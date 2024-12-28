4455c7723e2c070901f30b6dc05d528ed6f0958e
RAI2023 <r.ai.infused.2023@gmail.com>
2024-12-28 14:38:36 -0400

    Update Team_Coordination.py

--- features/Scripts/Team_Coordination.py
+++ features/Scripts/Team_Coordination.py
@@ -6,7 +6,7 @@ def feature_script():
     st.write("This app embeds a YouTube video and plays it within the browser.")
     st.write("⚠️ - Note : the video make take some seconds befote it plays.t")
 # YouTube video URL
-    video_url = "https://www.youtube.com/watch?v="
+    video_url = "https://www.youtube.com/watch?v=aSa8yn5ypl4"
 
 # Embed the video in the app
     st.video(video_url)
