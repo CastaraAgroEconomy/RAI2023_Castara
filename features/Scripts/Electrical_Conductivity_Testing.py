3f9cc58d763cafe2ff7e1c0d1c155571f4365ef8
RAI2023 <r.ai.infused.2023@gmail.com>
2024-12-28 18:09:41 -0400

    Update Electrical_Conductivity_Testing.py

--- features/Scripts/Electrical_Conductivity_Testing.py
+++ features/Scripts/Electrical_Conductivity_Testing.py
@@ -1,1 +1,16 @@
 # EC (Electrical Conductivity) testing launch pad
+
+import streamlit as st
+
+def feature_script():
+#   Description
+    st.write("This app embeds a YouTube video and plays it within the browser.")
+    st.write("⚠️ - Note : the video make take some seconds befote it plays.t")
+
+#   YouTube video URL
+    video_url = "https://www.youtube.com/watch?v="
+
+#   hEmbed the video in the app
+    st.video(video_url)
+
+    
