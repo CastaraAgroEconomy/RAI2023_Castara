33ab29d8aa181dce8159477c214899851536c3d7
RAI2023 <r.ai.infused.2023@gmail.com>
2024-12-28 18:23:43 -0400

    Update Default_Module_Name.py

--- features/Scripts/Default_Module_Name.py
+++ features/Scripts/Default_Module_Name.py
@@ -1,1 +1,16 @@
 # Dummy file for launchpad initalization
+
+import streamlit as st
+
+def feature_script():
+#   Description
+    st.write("This app embeds a YouTube video and plays it within the browser.")
+    st.write("⚠️ - Note : the video make take some seconds befote it plays it.")
+
+#   YouTube video URL
+    video_url = "https://www.youtube.com/watch?v="
+
+#   Embed the video in the app
+    st.video(video_url)
+
+    
