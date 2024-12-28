b2955300ad6a7b081e4b86450a02e4956f97b1db
RAI2023 <r.ai.infused.2023@gmail.com>
2024-12-28 18:40:50 -0400

    Update Equipment-Calibration.py

--- features/Scripts/Equipment-Calibration.py
+++ features/Scripts/Equipment-Calibration.py
@@ -5,7 +5,7 @@ import streamlit as st
 def feature_script():
 #   Description
     st.write("This app embeds a YouTube video and plays it within the browser.")
-    st.write("⚠️ - Note : the video make take some seconds before it play it.")
+    st.write("⚠️ - Note : the video make take some seconds before it plays.")
 
 #   YouTube video URL
     video_url = "https://www.youtube.com/watch?v="
