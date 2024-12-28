f02dd5b23a090a4e13fb3a03e086d7556dea0923
RAI2023 <r.ai.infused.2023@gmail.com>
2024-12-28 13:55:31 -0400

    Update Team_Coordination.py

--- features/Scripts/Team_Coordination.py
+++ features/Scripts/Team_Coordination.py
@@ -1,1 +1,14 @@
 # Team Coordination launch pad
+import streamlit as st
+
+def feature_script():
+#   Description
+    st.write("This app embeds a YouTube video and plays it within the browser.")
+    st.write("⚠️ - Note : the video make take some seconds befote it plays.t")
+# YouTube video URL
+    video_url = "https://www.youtube.com/watch?v="
+
+# Embed the video in the app
+    st.video(video_url)
+
+    return
