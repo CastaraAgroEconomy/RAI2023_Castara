6ba800a064ade79628a28d959f73813137674303
RAI2023 <r.ai.infused.2023@gmail.com>
2024-12-28 18:33:51 -0400

    Update Default_Module_Name.py

--- features/Scripts/Default_Module_Name.py
+++ features/Scripts/Default_Module_Name.py
@@ -5,7 +5,7 @@ import streamlit as st
 def feature_script():
 #   Description
     st.write("This app embeds a YouTube video and plays it within the browser.")
-    st.write("⚠️ - Note : the video make take some seconds befote it plays it.")
+    st.write("⚠️ - Note : the video make take some seconds before it plays.")
 
 #   YouTube video URL
     video_url = "https://www.youtube.com/watch?v="
