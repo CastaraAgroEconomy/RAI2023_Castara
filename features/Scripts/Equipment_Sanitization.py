2edf7aa468d2e9ea403c9136b3f1465d00ee9c82
RAI2023 <r.ai.infused.2023@gmail.com>
2024-12-29 05:15:47 -0400

    Update Equipment_Sanitization.py

--- features/Scripts/Equipment_Sanitization.py
+++ features/Scripts/Equipment_Sanitization.py
@@ -1,1 +1,15 @@
-# 
+# Equipment Sanitization feature launch pad
+import streamlit as st
+
+def feature_script():
+#   Description
+    st.write("This app embeds a YouTube video and plays it within the browser.")
+    st.write("⚠️ - Note : the video make take some seconds before it plays.")
+
+#   YouTube video URL
+    video_url = "https://www.youtube.com/watch?v="
+
+#   Embed the video in the app
+    st.video(video_url)
+
+    return
