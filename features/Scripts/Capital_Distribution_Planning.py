5d0eb84c4b5a2be44e7b659aa5b05a67b54cba96
RAI2023 <r.ai.infused.2023@gmail.com>
2024-12-29 05:07:26 -0400

    Update Capital_Distribution_Planning.py

--- features/Scripts/Capital_Distribution_Planning.py
+++ features/Scripts/Capital_Distribution_Planning.py
@@ -1,1 +1,15 @@
-# 
+# Capital Distribution Planning feature launch pad
+import streamlit as st
+
+def feature_script():
+#   Description
+    st.write("This app embeds a YouTube video and plays it within the browser.")
+    st.write("⚠️ - Note : the video make take some seconds before it plays.")
+
+#   YouTube video URL
+    video_url = "https://www.youtube.com/watch?v=PvCIl0C93zg"
+
+#   Embed the video in the app
+    st.video(video_url)
+
+    retur
