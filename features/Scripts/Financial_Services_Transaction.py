d26868447c5ebf73a2eae621b05a1d468b792715
RAI2023 <r.ai.infused.2023@gmail.com>
2024-12-30 01:58:23 -0400

    Update Financial_Services_Transaction.py

--- features/Scripts/Financial_Services_Transaction.py
+++ features/Scripts/Financial_Services_Transaction.py
@@ -1,1 +1,15 @@
-# 
+#    Financial Services Transaction feature launch pad
+import streamlit as st
+
+def feature_script():
+#   Description
+    st.write("This app embeds a YouTube video and plays it within the browser.")
+    st.write("⚠️ - Note : the video make take some seconds before it plays.")
+
+#   YouTube video URL
+    video_url = "https://www.youtube.com/watch?v="
+  
+#   Embed the video in the app
+    st.video(video_url)
+
+    return
