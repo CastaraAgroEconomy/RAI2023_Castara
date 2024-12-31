54946f91c0bfab2346d7b5862b5bce00fe4012fa
RAI2023 <r.ai.infused.2023@gmail.com>
2024-12-31 06:57:59 -0400

    Update CEO_Castara_AgroEconomy.py

--- CEO_Castara_AgroEconomy.py
+++ CEO_Castara_AgroEconomy.py
@@ -188,7 +188,8 @@ def main():
         
 #   Clear previous content
     content_clearspace = st.empty()  # Clear the placeholder    
-
+    content_clearspace.empty()
+    
     if (st.session_state.R_go == 1) and (st.session_state.live == 1):
 #   Display fearure header inside the placeholder - cleared screen
         with content_clearspace.container():
