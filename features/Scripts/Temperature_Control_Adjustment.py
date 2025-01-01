4314c9e7f1004c95245fc0ff92ddd9bf36f7ca2f
RAI2023 <r.ai.infused.2023@gmail.com>
2024-12-31 20:56:47 -0400

    Update Temperature_Control_Adjustment.py

--- features/Scripts/Temperature_Control_Adjustment.py
+++ features/Scripts/Temperature_Control_Adjustment.py
@@ -7,7 +7,7 @@ def feature_script():
     st.write("⚠️ - Note : the video make take some seconds before it plays.")
 
 #   YouTube video URL
-    video_url = https://www.youtube.com/watch?v=evX94tEQC1Q
+    video_url = https://www.youtube.com/watch?v=uWUAcc68MWI"
 
 #   Embed the video in the app
     st.video(video_url)
