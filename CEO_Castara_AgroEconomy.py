96f7097a35505a66db986aa637480a7b6fc15857
RAI2023 <r.ai.infused.2023@gmail.com>
2025-01-01 18:28:36 -0400

    Update CEO_Castara_AgroEconomy.py

--- CEO_Castara_AgroEconomy.py
+++ CEO_Castara_AgroEconomy.py
@@ -193,9 +193,11 @@ def main():
         if st.session_state.skip_final == 1:
 #   Clear the placeholder and launch the next feature
             launch_pad(content_placeholder)  # Pass the placeholder to the next function
+            pass
         elif st.button("Continue", on_click=lambda: " "):
 #   Set the session state to skip final
             st.session_state.skip_final = 1
+            pass
         
                               
         st.session_state.logged_in = True
