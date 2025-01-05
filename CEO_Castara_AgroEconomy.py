1af6bc06bf8481fa467da9785d43747756b0c478
RAI2023 <r.ai.infused.2023@gmail.com>
2025-01-05 01:31:18 -0400

    Update CEO_Castara_AgroEconomy.py

--- CEO_Castara_AgroEconomy.py
+++ CEO_Castara_AgroEconomy.py
@@ -216,7 +216,6 @@ def main():
             
 #   Display a message after the loop ends to restart from Job title entry           
             Display_clearer(content_placeholder)
-            st.write(" ")
             st.write("🚧 - Execution ended")
             st.session_state.stage = "sub_role_selection"
             
@@ -262,11 +261,10 @@ def main():
                 
                 st.session_state.live = 1  # Update the flag to stop the loop
                 st.session_state.logged_in = True             
-                st.session_state.do_not_skip = 0
+                st.session_state.do_not_skip = 1
 
 #   Display a message after the loop ends 
                 Display_Clearer(content_placeholder)
-                st.write(" ")
                 st.write("🚧 - Execution ended")
                 st.session_state.stage = "sub_role_selection"
                 
