2c2ee86d21c4b5a34139520d8317629334729f28
RAI2023 <r.ai.infused.2023@gmail.com>
2025-01-04 23:45:53 -0400

    Update CEO_Castara_AgroEconomy.py

--- CEO_Castara_AgroEconomy.py
+++ CEO_Castara_AgroEconomy.py
@@ -214,8 +214,7 @@ def main():
             st.session_state.logged_in = True           
             st.session_state.do_not_skip = 0
             
-# Display a message after the loop ends
-            
+# Display a message after the loop ends            
 # Specify the sub-folder where feature scripts are located    
            sub_folder = os.path.join("features", "Utility")
 
@@ -272,11 +271,18 @@ def main():
                 st.session_state.do_not_skip = 0
 
 
-# Display a message after the loop ends
-                Clear_screen()
-                st.write(" ")
-                st.write("🚧 - Execution ended")
-                st.session_state.stage = "sub_role_selection"
+# Display a message after the loop ends            
+# Specify the sub-folder where feature scripts are located    
+           sub_folder = os.path.join("features", "Utility")
+
+#   Add sub-folder to sys.path if not already present
+           if sub_folder not in sys.path:
+               sys.path.append(sub_folder)
+               
+            Clear_screen()
+            st.write(" ")
+            st.write("🚧 - Execution ended")
+            st.session_state.stage = "sub_role_selection"
 
     
 # ===================
