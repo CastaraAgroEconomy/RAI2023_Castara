38432959825defa9a423a84bec69bcecf7066ec5
RAI2023 <r.ai.infused.2023@gmail.com>
2025-01-20 02:49:33 -0400

    Update Compliance_Report_Generation.py

--- features/Scripts/Compliance_Report_Generation.py
+++ features/Scripts/Compliance_Report_Generation.py
@@ -6,7 +6,7 @@ st.write("This app embeds a YouTube video and plays it within the browser.")
 st.write("⚠️ Note: it may take a few seconds before the video begins to play.")
 
 # YouTube video URL
-video_url = "https://www.Youtube.com/watch?v=PvCIl0C93zg"
+video_url = "https://www.Youtube.com/watch?v=v08sZvHyMtw"
 
 # Embed the video in the app
 st.video(video_url)
